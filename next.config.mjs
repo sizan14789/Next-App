eb6e18f6f8c1db6fe3927e993ac519df022767da
sizan14789 <sizanalt@gmail.com>
2025-02-02 21:51:53 +0600

    gotta fix database

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://next-cu6ouww11-sizan14789s-projects.vercel.app/" 
+  },
   images:{
     domains:[
       "images.pexels.com"
